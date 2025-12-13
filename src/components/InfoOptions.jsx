df341e13e825b9f788d4598d5be4065dc12eb647
ashreekar <ashreekar767@gmail.com>
2025-12-13 21:58:09 +0530

    add toggle projects and education

---
+++ src/components/InfoOptions.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function InfoOptions({ option, setOption }) {
+    const selectedClass="bg-white shadow-md rounded-md h-full w-1/2 text-center p-1 text-gray-800 font-semibold transition duration-300 cursor-pointer"
+    const outClass="h-full w-1/2 text-center font-semibold text-gray-700 p-1 transition duration-300 cursor-pointer"
+    return (
+        <div className='bg-gray-100 flex p-1 rounded-md gap-2'>
+            <p className={option==="projects"?selectedClass:outClass} onClick={()=>setOption("projects")}>Projects</p>
+            <p className={option==="education"?selectedClass:outClass} onClick={()=>setOption("education")}>Education</p>
+        </div>
+    )
+}
+
+export default InfoOptions
--- src/components/MainInfo.jsx
+++ src/components/MainInfo.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import InfoOptions from './InfoOptions'
 
 function MainInfo() {
+    const [option,setOption]=useState("projects");
+
   return (
-    <div>MainInfo</div>
+    <div className="mx-auto mt-4 w-full lg:w-1/2 px-6">
+        <InfoOptions option={option} setOption={setOption} />
+    </div>
   )
 }
 
